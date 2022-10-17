510eee721c9fff93a5f59721932c6bb87bdc4755
trinh <trinhrob123@gmail.com>
2022-10-17 20:06:13 +0700

    add first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Admin from "./features/components/Admin";
+import Customer from "./features/components/Customer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Admin />} /> 
+          <Route path="/customer" element={<Customer />} /> 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
